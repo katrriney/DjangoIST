03dc84319396686b9b2ab28736b9d6f91668159c
Екатерина <katya.sentyurina@gmail.com>
2023-10-29 14:42:24 +0300

    4 лаб

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -8,18 +8,18 @@ register_converter(FourDigitYearConverter, "yyyy")
 urlpatterns = [
 
     path('', index, name='home'),
+    path('about/', about, name='about'),
     path('cat/', categories, name='category'),
     path("articles/<yyyy:year>/",year, name='articles'),
     path('cat/<int:catid>/', categories_id, name='cat'),
     path('cat/<slug:catid>/', categories_sl, name='cat_slug'),
     path('students/<int:students_id>/', students, name='students'),
     path('students/<slug:students>/', students_slug),
-    path("spisok/<int:key> ",spisok),
-    path('date/<int:datee>/',date),
-    path('stup/<int:axe>/',stup),
+    path("spisok/<int:key> ", spisok),
+    path('date/', date, name='date'),
+    path('stup/<int:axe>/', stup),
     path('GET/', getStr, name='getStr'),
     path('GET_1/', post_detail, name='post_detail'),
-
-
+    path('cross/', cross, name='cross'),
 ]
 
